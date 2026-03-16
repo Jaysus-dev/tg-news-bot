7646a1606d0c7c3b8b0928c4f32014dc7856ab2e
jaysus-dev <edg.estoquia2@gmail.com>
2026-03-16 13:13:32 +0800

    update: scheduler send

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,7 +10,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
        // $schedule->command('news:send')->everyMinute();
-        $schedule->command('news:send')->dailyAt('01:30');
+        $schedule->command('news:send')->dailyAt('09:00');
     }
 
     protected function commands(): void
